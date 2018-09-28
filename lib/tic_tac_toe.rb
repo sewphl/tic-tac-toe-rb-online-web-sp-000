abb80bf0265f490904d330b5a41598e56f7a720f
sharon <sharonoptsout@gmail.com>
2018-09-28 19:47:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -119,7 +119,7 @@ def play(board)
   iwon = winner(board)
   if iwon != false
     puts "Congratulations #{iwon}!"
-  elsif draw?(board)
+  elsif draw?(board) == true
     puts "Cat's Game!"
   end
 end
